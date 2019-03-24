4fa63a8926536e0927ca68351410e1e0dde6b0f7
yaroslav-codefresh <yaroslav@codefresh.io>
2019-03-24 20:52:57 +0200

    add endpoints + listeners

---
+++ defaults.js
@@ -0,0 +1,6 @@
+module.exports = {
+    REDOC_STATIC_PATH: '/api/static/redoc.standalone.min.js',
+    SPEC_FILENAME: './openapi.json',
+    SPEC_PATH: '/api/openapi.json',
+    ADMIN_SPEC_PATH: '/api/admin/openapi.json',
+};
