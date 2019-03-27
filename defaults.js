eb719baec4283a2ea53c9d7e364cc89d74ff1175
yaroslav-codefresh <yaroslav@codefresh.io>
2019-03-27 18:04:53 +0200

    instances support + new components

--- defaults.js
+++ defaults.js
@@ -1,6 +1,9 @@
 module.exports = {
-    REDOC_STATIC_PATH: '/api/static/redoc.standalone.min.js',
     SPEC_FILENAME: './openapi.json',
     SPEC_PATH: '/api/openapi.json',
     ADMIN_SPEC_PATH: '/api/admin/openapi.json',
+    REDOC_PATH: '/api',
+    ADMIN_REDOC_PATH: '/api/admin',
+    REDOC_STATIC_PATH: '/api/static/redoc.standalone.min.js',
+    DEPENDENCIES_FETCH_RETRY_TIMEOUT: process.env.DEPENDENCIES_FETCH_RETRY_TIMEOUT || 5000,
 };
