9e3d1c1ace1886b3bda9e11b4a4c6165859430a6
yaroslav-codefresh <yaroslav@codefresh.io>
2019-04-09 11:33:41 +0300

    remove build routes + add endpoints validation + no messages in case of no endpoints

---
+++ bin/endpointsValidator.js
@@ -0,0 +1,15 @@
+const _ = require('lodash');
+const { endpoints, spec } = require('../lib/components');
+const { findAppRoot } = require('../lib/helpers/general');
+
+function endpointsValidator(openapiJson) {
+    process.env.FIREBASE_SECRET = 'test';
+    const appRoot = findAppRoot(process.cwd());
+    endpoints.appRoot = appRoot;
+    spec.appRoot = appRoot;
+    endpoints.loadEndpoints();
+    spec.set(openapiJson);
+    endpoints.validateAppEndpoints();
+}
+
+module.exports = endpointsValidator;
